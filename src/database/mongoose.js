e9ee4f998ce6fb97e8360948dce7df64d394c605
Jose <Jose0156.114@gmail.com>
2024-12-06 14:52:34 -0600

    actualizacion conexion DB

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -7,15 +7,11 @@ const uri = process.env.MONGO_URI; // Leer la URI desde el archivo .env
 
 const connectDB = async () => {
     try {
-        // Conectar a MongoDB con las opciones recomendadas
-        await mongoose.connect(uri, {
-            useNewUrlParser: true, // Usa el nuevo parser de URLs
-            useUnifiedTopology: true, // Nueva gestión de conexiones
-        });
+        await mongoose.connect(uri); // Conectar a MongoDB
         console.log("✅ Conexión a MongoDB Atlas exitosa");
     } catch (error) {
         console.error("❌ Error al conectar a MongoDB Atlas:", error);
-        process.exit(1); // Detiene el proceso si falla la conexión
+        process.exit(1);
     }
 };
 
