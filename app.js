cf4d2de1ffd28aabbf77f7886a66afa308c70cb9
Jose <Jose0156.114@gmail.com>
2024-12-06 14:46:32 -0600

    actualizacion archivos estaticos

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require("path"); // Importar módulo para manejar rutas
 // middlewares
 app.use(express.json()); //solicitudes con cuerpo en formato JSON
 app.use(express.urlencoded({ extended: true })); // solicitudes con cuerpo en formato URL-encoded
-app.use(express.static(path.join(__dirname, "public"))); // Servir archivos estaticos
+app.use(express.static(path.join(__dirname, "./src/public"))); // Servir archivos estaticos
 
 // ruta de tickets
 app.use("/api/tickets", ticketRoutes); // Usar rutas bajo el prefijo /api/tickets
